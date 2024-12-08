11969eb42cc1581c4ca8a6b4c5ed13d1a81bc4a4
Belkys Lopez <belkiscarolina1@hotmail.com>
2024-12-07 21:41:41 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'app-random-img',
+  webDir: 'www'
+};
+
+export default config;
